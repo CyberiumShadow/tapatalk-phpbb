4daf9783a868a103ab9a1fb2b551b2626fdcb312
Johnson Chen <cyberiumdev@gmail.com>
2022-11-06 12:58:30 +0000

    add user groups

--- conversion/src/functions/members.ts
+++ conversion/src/functions/members.ts
@@ -7,6 +7,7 @@ export default async function mapMembers() {
 
     for (const memberidx in TTMembers) {
         const member = TTMembers[memberidx]
+        const member_group: String[] = JSON.parse(member.group!)
         console.log(`ID: ${member.id} | Name: ${member.name}`)
         await phpbbClient.user.create({
             data: {
@@ -16,8 +17,18 @@ export default async function mapMembers() {
                 user_regdate: member.joined || undefined,
                 user_avatar: member.avatarremote || '',
                 user_avatar_type: member.avatarremote ? 1 : 0,
-                user_ip: member.ip || ''
+                user_ip: member.ip || '',
+                group_id: Number(member_group[member_group.length - 1])
             }
         })
+        for (const groupidx in member_group) {
+            const group = Number(member_group[groupidx])
+            await phpbbClient.userGroup.create({
+                data: {
+                    group_id: group,
+                    user_id: member.id,
+                }
+            })
+        }
     }
 }
